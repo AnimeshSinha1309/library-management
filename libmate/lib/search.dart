760d2c3547a5e22b3f71aa2e76010f4d3299ad17
Animesh Sinha <animeshsinha1309@gmail.com>
2020-03-10 08:56:01 +0530

    An infinite Scroll view is spwaned
    Need more visual and backend, but here is a item-builder that gives infinite scroll.

--- libmate/lib/search.dart
+++ libmate/lib/search.dart
@@ -14,6 +14,47 @@ class _SearchPageState extends State<SearchPage> {
       appBar: new AppBar(
         title: new Text('Search Page'),
       ),
+      body: new ListView.builder(
+        padding: const EdgeInsets.all(16),
+        /// Scrolling List of results
+        itemBuilder: (context, i) {
+          return Container(
+            height: 130,
+            child: Card(
+              elevation: 3,
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        return;
+                      },
+                      child: Container(
+                        width: 100.0,
+                        height: 100.0,
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      return;
+                    },
+                    child: Container(
+                        padding: EdgeInsets.all(30.0),
+                        child: Chip(
+                          label: Text('@book'),
+                          backgroundColor: Colors.yellow,
+                          elevation: 2,
+                          autofocus: true,
+                        )),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
       drawer: new AppDrawer(),
     );
   }

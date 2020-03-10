892a4e37547d7ceeb5a8e061fe361eeac618b9d9
Avani Gupta <staravani1999@gmail.com>
2020-03-11 00:42:55 +0530

    Update title_isbn.py

--- dev/metadata/title_isbn.py
+++ dev/metadata/title_isbn.py
@@ -1,4 +1,5 @@
 import requests as req
+import json
 
 def getBookDetails(title):
 
@@ -6,22 +7,23 @@ def getBookDetails(title):
     response = req.get(url)
     results = response.json()
     print(results)
-    if results["totalItems"]:
-        book = results["items"][0]
-        title = book["volumeInfo"]["title"]
-        categories = book["volumeInfo"]["categories"]
-        authors = book["volumeInfo"]["authors"]
-        printType = book["volumeInfo"]["printType"]
-        pageCount = book["volumeInfo"]["pageCount"]
-        ratingsCount = book["volumeInfo"]["ratingsCount"]
-        averageRating = book["volumeInfo"]["averageRating"]
-        publisher = book["volumeInfo"]["publisher"]
-        publishedDate = book["volumeInfo"]["publishedDate"]
-        previewLink = book["volumeInfo"]["previewLink"]
-        thumbnail = book["volumeInfo"]["imageLinks"]["thumbnail"]
-        language = book["volumeInfo"]["language"]
-        description = book["volumeInfo"]["description"]
-        webReaderLink = book["accessInfo"]["webReaderLink"]
+    # if results["totalItems"]:
+    #     book = results["items"][0]
+    #     isbn = book["volumeInfo"]["isbn"]
+    #     categories = book["volumeInfo"]["categories"]
+    #     authors = book["volumeInfo"]["authors"]
+    #     printType = book["volumeInfo"]["printType"]
+    #     pageCount = book["volumeInfo"]["pageCount"]
+    #     ratingsCount = book["volumeInfo"]["ratingsCount"]
+    #     averageRating = book["volumeInfo"]["averageRating"]
+    #     publisher = book["volumeInfo"]["publisher"]
+    #     publishedDate = book["volumeInfo"]["publishedDate"]
+    #     previewLink = book["volumeInfo"]["previewLink"]
+    #     thumbnail = book["volumeInfo"]["imageLinks"]["thumbnail"]
+    #     language = book["volumeInfo"]["language"]
+    #     description = book["volumeInfo"]["description"]
+    #     webReaderLink = book["accessInfo"]["webReaderLink"]
+    
 
 
 if __name__ == '__main__':

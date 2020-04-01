7b6ae0fe1413397cc0a78eaab945db2f57131ea0
Animesh Sinha <animeshsinha1309@gmail.com>
2020-04-02 04:15:23 +0530

    WIP: A form for new books
    Just put a a plain form for new books with basic validation.

--- libmate/lib/request.dart
+++ libmate/lib/request.dart
@@ -8,6 +8,8 @@ class RequestPage extends StatefulWidget {
 }
 
 class _RequestPageState extends State<RequestPage> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +17,75 @@ class _RequestPageState extends State<RequestPage> {
         title: new Text('Request Page'),
       ),
       drawer: AppDrawer(),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(hintText: "Name of the Book"),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(hintText: "Estimated Price"),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(hintText: "Genre / Subject"),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(hintText: "Reasons, Cosigners, etc."),
+                maxLines: 3,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: RaisedButton(
+                  color: Colors.pinkAccent,
+                  onPressed: () {
+                    // Validate returns true if the form is valid, or false
+                    // otherwise.
+                    if (_formKey.currentState.validate()) {
+                      // If the form is valid, display a Snackbar.
+                      Scaffold.of(context).showSnackBar(SnackBar(
+                        content: Text('Backend for the App is not Ready'))
+                      );
+                    }
+                  },
+                  child: Text('Submit'),
+                ),
+              ),
+            ],
+          ),
+        )
+      )
     );
   }
 }

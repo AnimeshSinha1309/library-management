05ce7ea89455213a253ad7bfbe172591c89c4334
avani17101 <staravani1999@gmail.com>
2020-07-15 20:33:31 +0530

    custom signin-auth

--- libmate/lib/services/auth.dart
+++ libmate/lib/services/auth.dart
@@ -4,10 +4,25 @@ import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:libmate/datastore/model.dart';
 import 'package:libmate/views/sign_in.dart';
+import 'package:libmate/models/user.dart';
 
 
 class AuthService{
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  User _userFromFirebaseUser(FirebaseUser user){
+    return user != null ? User(uid: user.uid): null;
+  }
+
+  Future registerWithEmailAndPassword(String email, String password) async{
+    try{
+      AuthResult result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch(e){
+      print(e.toString());
+      return null;
+    }
+  }
   //signin anon
   Future signInAnon() async{
     try{
--- libmate/lib/views/register.dart
+++ libmate/lib/views/register.dart
@@ -11,8 +11,10 @@ class Register extends StatefulWidget {
 
 class _RegisterState extends State<Register> {
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
   String email = "";
   String password = "";
+  String error = "";
 
   @override
   Widget build(BuildContext context) {
@@ -38,10 +40,12 @@ class _RegisterState extends State<Register> {
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: RaisedButton(
           child: Form(
+            key: _formKey,
             child: Column(
               children: <Widget>[
                 SizedBox(height: 20.0),
                 TextFormField(
+                    validator: (val) => val.isEmpty ? "Enter an email" : null,
                     onChanged: (val){
                       setState(() => email = val);
                     }
@@ -49,22 +53,28 @@ class _RegisterState extends State<Register> {
                 SizedBox(height: 20.0),
                 TextFormField(
                     obscureText: true,
+                    validator: (val) => val.length < 6 ? "Enter a password 6+ chars long" : null,
                     onChanged: (val){
                       setState(() => password = val);
                     }
                 ),
                 SizedBox(height: 20.0),
+
                 RaisedButton(
                   color: Colors.pink[400],
                   child: Text(
                     "Register",
                     style: TextStyle(color: Colors.white),
                   ),
-                  onPressed: () async{
-                    print(email);
-                    print(password);
-                  },
-                )
+                  onPressed: () async {
+                    if (_formKey.currentState.validate()) {
+                      dynamic result = await _auth.registerWithEmailAndPassword(
+                          email, password);
+                      if (result == null) {
+                        setState(() => error = 'Please supply a valid email');
+                      }
+                    }
+                  })
               ],
             ),
           ),

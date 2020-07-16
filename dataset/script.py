2c0ae47c98d4147a79365e32cbbf872f90d42f46
Akshat Goyal <akshatgoyalak23@gmail.com>
2020-07-16 23:04:04 +0530

    script corrected

--- dataset/script.py
+++ dataset/script.py
@@ -8,27 +8,28 @@ book_list = []
 label = 0
 
 for folder in os.listdir(base_url):
-    folder_path = os.path.join(base_url, folder)
-    for file in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, file)
-        with open(file_path) as f:
-            books = json.load(f)
-            for book in books:
-                book = book['volumeInfo']
-                title = book['title'] if 'title' in book else ""
-                description = book['description'] if 'description' in book else ""
-                if 'authors' in book:
-                    for author in book['authors']:
-                        authors += author + ", "
-                    authors = authors.strip(', ')
-                else:
-                    authors = ''
-                text = title + " " + authors + " " description
-                text = text.strip(' ')
-                book_list.append({'label': label, 'category': folder, 'subcategory': file.split('.')[0], 'text': text })
-        label += 1
-        
+	folder_path = os.path.join(base_url, folder)
+	for file in os.listdir(folder_path):
+		file_path = os.path.join(folder_path, file)
+		with open(file_path) as f:
+			books = json.load(f)
+			for book in books:
+				book = book['volumeInfo']
+				title = book['title'] if 'title' in book else ""
+				description = book['description'] if 'description' in book else ""
+				if 'authors' in book:
+					authors = ''
+					for author in book['authors']:
+						authors += author + ", "
+					authors = authors.strip(', ')
+				else:
+					authors = ''
+				text = title + " " + authors + " " + description
+				text = text.strip(' ')
+				book_list.append({'label': label, 'category': folder, 'subcategory': file.split('.')[0], 'text': text })
+		label += 1
+		
 with open('book_dataset.csv', 'w') as f:
-    w = csv.DictWriter(f, book_list[0].keys())
-    w.writeheader()
-    w.writerows(book_list)
+	w = csv.DictWriter(f, book_list[0].keys())
+	w.writeheader()
+	w.writerows(book_list)

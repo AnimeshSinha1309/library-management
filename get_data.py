ae882cb45994d578ee1c6fc05de782dcabd364d3
arpan-dasgupta <arpandasgupta0@gmail.com>
2020-07-19 17:04:41 +0530

    Updated Arpan's files

--- get_data.py
+++ get_data.py
@@ -1,16 +1,30 @@
 import json
+import os
 from requests import get
 
 subjects = ["Anatomy", "Biochemistry", "Biotechnology",
             "Botany", "Ecology", "Genetics", "Physiology", "Zoology"]
 # subject = 'American History'
+folders = ['History', 'Crafts & Hobbies',
+           'Home & Garden', 'Horror', 'Kids', 'Biology']
 
-for subject in subjects:
-    url = 'https://www.googleapis.com/books/v1/volumes?q=subject:' + \
-        subject + '&langRestrict=en&startIndex=0&maxResults=40'
-    res = get(url).json()
-    if res['totalItems'] == 0:
-        continue
-    print(res)
-    with open(subject+'.json', 'w') as f:
-        json.dump(res, f)
+for folder in folders:
+    subjects = os.listdir('dataset/topics/'+folder)
+    subjects = [s.split(sep='.')[0] for s in subjects]
+
+    for subject in subjects:
+        arr = []
+        for times in range(3):
+            url = 'https://www.googleapis.com/books/v1/volumes?q=subject:' + \
+                subject + '&langRestrict=en&startIndex=' + \
+                str(40*times)+'&maxResults=40'
+            res = get(url).json()
+            if not res['totalItems']:
+                break
+            arr += res['items']
+            if len(res['items']) < 40:
+                break
+        print(arr)
+        if len(arr) != 0:
+            with open('dataset/topics/'+folder+'/'+subject+'.json', 'w') as f:
+                json.dump(arr, f)

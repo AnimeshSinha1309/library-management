378dd5d0846e01d5a5103c7e4243dfbd1129ba18
Animesh Sinha <animeshsinha1309@gmail.com>
2020-07-27 16:56:04 +0530

    The Old Book Cards
    Sizing is all wrong, the but the book cards are prettier (in my opinion).

--- libmate/lib/widgets/bookcard.dart
+++ libmate/lib/widgets/bookcard.dart
@@ -1,94 +1,107 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:libmate/datastore/model.dart';
-import 'package:libmate/views/drawer.dart';
 import 'package:libmate/utils/utils.dart';
+import 'package:libmate/views/drawer.dart';
 import 'package:libmate/widgets/toread.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:intl/intl.dart';
-import 'dart:convert';
 
 class BookCard extends StatelessWidget {
   final BookModel model;
   bool shouldOpenPage;
 
-  BookCard({Key key, @required this.model, this.shouldOpenPage})
-      : super(key: key) {
-    shouldOpenPage = shouldOpenPage ?? false;
-  }
+  BookCard({@required this.model, this.shouldOpenPage = true})
+      : super(key: UniqueKey()) {}
 
   @override
   Widget build(BuildContext context) {
     final double height = 200;
 
     return Card(
-      elevation: 5,
-      child: InkWell(
-          splashFactory: InkRipple.splashFactory,
-          splashColor: Colors.white,
-          onTap: () {
-            if (shouldOpenPage) gotoPage(context, BookPage(model: model));
-          },
-          child: Row(children: [
-            Expanded(
-              flex: 3,
-              child: Container(
-                  height: height,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage(model.image),
-                      fit: BoxFit.fitHeight,
+        elevation: 5,
+        child: SizedBox(
+          height: 300,
+          width: 200,
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(model.image),
+                fit: BoxFit.fitWidth,
+                alignment: Alignment.topCenter,
+              ),
+            ),
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: FractionallySizedBox(
+                  heightFactor: 0.6,
+                  widthFactor: 1.0,
+                  child: Material(
+                    color: Color.fromRGBO(0, 0, 0, 0.9),
+                    child: InkWell(
+                      splashFactory: InkRipple.splashFactory,
+                      splashColor: Colors.white,
+                      onTap: () {
+                        if (shouldOpenPage)
+                          gotoPage(context, BookPage(model: model));
+                      },
+                      child: Container(
+                        padding: EdgeInsets.all(8),
+                        color: Color.fromRGBO(0, 0, 0, 0.9),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              model.subject ?? "",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              model.genre ?? "",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                            Spacer(),
+                            Text(
+                              model.author ?? "",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                            Spacer(),
+                            Text(
+                              "Genre: " + model.genre ?? "",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                            Text(
+                              "ISBN: " + model.isbn ?? "",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   )),
             ),
-            Expanded(
-              flex: 4,
-              child: Container(
-                height: height,
-                padding: EdgeInsets.all(8),
-                color: Color.fromRGBO(100, 100, 100, 0.9),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      model.name,
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Spacer(),
-                    Text(
-                      model.author,
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                    Spacer(),
-                    Text(
-                      "Genre: " + model.genre,
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                    Text(
-                      "ISBN: " + model.isbn,
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ])),
-    );
+          ),
+        ));
   }
 }
 
 class BookPage extends StatelessWidget {
   BookModel model;
   final int maxBooks = 20;
+
   BookPage({@required this.model});
 
   Future<int> saveReadingList(BookModel model) async {
@@ -97,7 +110,7 @@ class BookPage extends StatelessWidget {
     if (readlist.length > maxBooks) return 1;
 
     ToRead rbook = new ToRead();
-    rbook.book = model.name;
+    rbook.book = model.name ?? "";
     rbook.date = new DateFormat("dd/MM").format(DateTime.now()).toString();
 
     String sbook = json.encode(rbook);
@@ -135,15 +148,15 @@ class BookPage extends StatelessWidget {
         drawer: AppDrawer(),
         body: Builder(
             builder: (context) => Column(children: [
-                  BookCard(model: model),
-                  Text("Copies: available 5, total 10"),
-                  RaisedButton(
-                    onPressed: () async {
-                      final String resp = await addBook(model);
-                      showToast(context, resp);
-                    },
-                    child: Text("Add to reading list"),
-                  )
-                ])));
+              BookCard(model: model),
+              Text("Copies: available 5, total 10"),
+              RaisedButton(
+                onPressed: () async {
+                  final String resp = await addBook(model);
+                  showToast(context, resp);
+                },
+                child: Text("Add to reading list"),
+              )
+            ])));
   }
 }

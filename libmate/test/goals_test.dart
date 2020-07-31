27818cd4281559cc265888fa5dfea034c72012f3
Arpan Dasgupta <arpandasgupta0@gmail.com>
2020-08-01 01:14:12 +0530

    Set up testing

---
+++ libmate/test/goals_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:libmate/views/goals.dart';
+import 'package:libmate/widgets/toread.dart';
+
+void main() {
+  group('Goals', () {
+    test('Initially should be zero', () {
+      final goals = GoalsPage();
+
+      expect(goals.createState().numBooks, 0);
+    });
+    // test('Adding', () {
+    //   final goals = GoalsPage().createState();
+    //   goals.addBook(ToRead(book: "New Book 1", date: "28/7"));
+    //   goals.addBook(ToRead(book: "New Book 2", date: "28/8"));
+    //   expect(goals.numBooks, 2);
+    // });
+    // test('Removal', () {
+    //   final goals = GoalsPage().createState();
+    //   goals.addBook(ToRead(book: "New Book 1", date: "28/7"));
+    //   goals.addBook(ToRead(book: "New Book 2", date: "28/8"));
+    //   goals.removeBook(ToRead(book: "New Book 2", date: "28/8"));
+    //   expect(goals.numBooks, 1);
+    // });
+  });
+}

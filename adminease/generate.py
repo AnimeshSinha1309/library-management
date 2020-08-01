84e7ecdbebf5684728685d1ee60bdd9275d60b53
Animesh Sinha <animeshsinha1309@gmail.com>
2020-08-01 16:51:49 +0530

    All the Barcode in one page
    Merge several barcodes into a single printable.

--- adminease/generate.py
+++ adminease/generate.py
@@ -1,4 +1,28 @@
+from PIL import Image
 import barcode
+import os
+import numpy as np
 
 if __name__ == '__main__':
-    ean = barcode.lib.LibraryBookIdentifier.generate(120, 1012, 'sample_book_code')
+    books = [(np.random.randint(100), np.random.randint(1000)) for i in range(21)]
+
+    width, height = int(8.27 * 300), int(11.7 * 300) # A4 at 300dpi
+    blank_image = Image.new("RGB", (width, height))
+
+    rows, cols = 8, 4
+    w, h = width // cols, height // rows
+
+    y = 0
+    for i in range(rows):
+        x = 0
+        for j in range(cols):
+            if i + j * cols >= len(books): continue
+            ean = barcode.lib.LibraryBookIdentifier.generate(books[i + j * cols][0], books[i + j * cols][1], 'temporary_barcode')
+            image = Image.open("temporary_barcode.png")
+            image = image.resize((w, h), resample=0)
+            blank_image.paste(image,(x, y))
+            x += w
+        y += h
+    
+    blank_image.save("print_barcodes.png")
+    os.remove("temporary_barcode.png")

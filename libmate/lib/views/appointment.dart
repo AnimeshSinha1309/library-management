de7d1c88caf96858f1ec9f0c3325e48b19dd3b77
Akshat Goyal <akshatgoyalak23@gmail.com>
2020-08-01 21:52:31 +0530

    firestore database added, overflow error solved, need to modify database fields and scheduling algo

--- libmate/lib/views/appointment.dart
+++ libmate/lib/views/appointment.dart
@@ -2,25 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:libmate/views/drawer.dart';
 import 'package:libmate/views/timeslots.dart';
 
-
 class AppointmentPage extends StatefulWidget {
   @override
   _AppointmentPageState createState() => _AppointmentPageState();
 }
 
-class _AppointmentPageState extends State<AppointmentPage > {
-
+class _AppointmentPageState extends State<AppointmentPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: new AppBar(
           title: new Text('Appointment Scheduler'),
           centerTitle: true,
-
         ),
         drawer: AppDrawer(),
         body: Padding(
-            padding: EdgeInsets.fromLTRB(30.0,40.0,30.0,0.0),
+            padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
             child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
@@ -29,46 +26,42 @@ class _AppointmentPageState extends State<AppointmentPage > {
                       style: TextStyle(
                         color: Colors.grey,
                         letterSpacing: 2.0,
-                      )
-                  ),
-                  Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                    Center(
-                      child: ButtonTheme(
-                        textTheme: ButtonTextTheme.primary,
-                        child: RaisedButton.icon(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => TimePage()),
-                            );
-                          },
-                          icon: Icon(Icons.assignment_return),
-                          label: Text('Issue/Return book'),
+                      )),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Expanded(
+                          child: ButtonTheme(
+                            textTheme: ButtonTextTheme.primary,
+                            child: RaisedButton.icon(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => TimePage()),
+                                );
+                              },
+                              icon: Icon(Icons.assignment_return),
+                              label: Text('Issue / Return\n       book'),
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                     Center(
-                        child: ButtonTheme(
+                        SizedBox(width: 4),
+                        ButtonTheme(
                           textTheme: ButtonTextTheme.primary,
                           child: RaisedButton.icon(
                             onPressed: () {
                               Navigator.push(
                                 context,
-                                MaterialPageRoute(builder: (context) => TimePage()),
+                                MaterialPageRoute(
+                                    builder: (context) => TimePage()),
                               );
                             },
                             icon: Icon(Icons.person_pin),
                             label: Text('Sit in Library'),
                           ),
-                        )),
-                  ]),
-
-
-                ]
-            )
-        )
-
-    );
+                        )
+                      ]),
+                ])));
   }
 }
--- libmate/lib/views/timeslots.dart
+++ libmate/lib/views/timeslots.dart
@@ -1,40 +1,142 @@
 import 'package:flutter/material.dart';
 import 'package:libmate/views/drawer.dart';
 import 'package:libmate/views/ticket.dart';
+import 'package:libmate/utils/utils.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:async';
+
 class TimePage extends StatefulWidget {
   @override
   _TimePageState createState() => _TimePageState();
 }
 
 class _TimePageState extends State<TimePage> {
-  Iterable<TimeOfDay> getTimes(TimeOfDay startTime, TimeOfDay endTime, Duration step) sync* {
-    var hour = startTime.hour;
-    var minute = startTime.minute;
+  bool _loaded = false;
+  Timer _timer;
+  int _maxUSers = 10;
+  Set<String> _slots = Set();
 
-    do {
-      yield TimeOfDay(hour: hour, minute: minute);
-      minute += step.inMinutes;
-      while (minute >= 60) {
-        minute -= 60;
-        hour++;
+  Future getAppointments() async {
+    try {
+      final snapShot =
+          await Firestore.instance.collection('appointments').getDocuments();
+      Set<String> _slotlist = Set();
+      for (var document in snapShot.documents) {
+        if (document.data['cnt'] >= _maxUSers) {
+          _slotlist.add(document.documentID);
+        }
       }
-    } while (hour < endTime.hour ||
-        (hour == endTime.hour && minute <= endTime.minute));
+      setState(() {
+        _slots = _slotlist;
+        _loaded = true;
+      });
+    } catch (e) {
+      print(e.toString());
+    }
   }
-  void genslots(startTime,endTime){
-    final step = Duration(minutes: 30);
 
-    final times = getTimes(startTime, endTime, step)
-        .map((tod) => tod.format(context))
-        .toList();
-
-    print(times);
+  @override
+  void initState() {
+    super.initState();
+    getAppointments();
+    _timer = Timer.periodic(Duration(seconds: 60), (Timer t) {
+      getAppointments();
+    });
   }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  // Iterable<TimeOfDay> getTimes(
+  //     TimeOfDay startTime, TimeOfDay endTime, Duration step) sync* {
+  //   var hour = startTime.hour;
+  //   var minute = startTime.minute;
+
+  //   do {
+  //     yield TimeOfDay(hour: hour, minute: minute);
+  //     minute += step.inMinutes;
+  //     while (minute >= 60) {
+  //       minute -= 60;
+  //       hour++;
+  //     }
+  //   } while (hour < endTime.hour ||
+  //       (hour == endTime.hour && minute <= endTime.minute));
+  // }
+
+  // void genslots(startTime, endTime) {
+  //   final step = Duration(minutes: 30);
+
+  //   final times = getTimes(startTime, endTime, step)
+  //       .map((tod) => tod.format(context))
+  //       .toList();
+
+  //   print(times);
+  // }
 //  final startTime = TimeOfDay(hour: 9, minute: 0);
 //  final endTime = TimeOfDay(hour: 22, minute: 0);
 //  genslots(startTime,endTime);
 
-  List<String> times = ['9:30 AM','10:00 AM','10:30 AM','11:00 AM','11:30 AM'];
+  List<String> times = [
+    '2020-08-01_09:30',
+    '2020-08-01_10:00',
+    '2020-08-01_10:30',
+    '2020-08-01_11:00',
+    '2020-08-01_11:30'
+  ];
+
+  Widget _timeCell(String slot) {
+    bool _isAvailable = !_slots.contains(slot);
+
+    return TableCell(
+        child: Center(
+      child: RaisedButton(
+        color: _isAvailable == true ? Colors.green : Colors.red,
+        onPressed: () async {
+          if (_isAvailable) {
+            var docRef =
+                Firestore.instance.collection('appointments').document(slot);
+            Firestore.instance.runTransaction((transaction) {
+              return transaction.get(docRef).then((doc) {
+                if (doc.exists) {
+                  var cnt = doc.data['cnt'];
+                  if (cnt < _maxUSers) {
+                    transaction.update(docRef, {
+                      'cnt': cnt + 1,
+                    });
+                  } else {
+                    _isAvailable = false;
+                  }
+                } else {
+                  transaction.set(docRef, {
+                    'cnt': 1,
+                  });
+                }
+              });
+            });
+          }
+          if (_isAvailable) {
+            setState(() {
+              _slots.add(slot);
+            });
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => TicketPage()),
+            );
+          } else {
+            showToast(context, 'Sorry, This slot is not available!');
+          }
+        },
+        child: Text(
+          slot.split('_')[1],
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,95 +146,22 @@ class _TimePageState extends State<TimePage> {
           centerTitle: true,
         ),
         drawer: AppDrawer(),
-        body:
-
-        Padding(
-
-        padding: const EdgeInsets.all(8.0),
-        child: Table(
-
-            //          defaultColumnWidth:
-            //              FixedColumnWidth(MediaQuery.of(context).size.width / 3),
-                border: TableBorder.all(
-                color: Colors.black26, width: 1, style: BorderStyle.none),
-                children: [
-
-                TableRow(children: [
-                TableCell(child: Center(child: RaisedButton(
-                              color: Colors.red,
-                              onPressed: (){},
-                              child: Text(
-                              '11 am',
-                              style: TextStyle(color: Colors.white),
-                              ),
-                              ),)),
-                  TableCell(child: Center(child: RaisedButton(
-                    color: Colors.green,
-                    onPressed: (){},
-                    child: Text(
-                      '12 am',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),)),
-                  TableCell(child: Center(child: RaisedButton(
-                    color: Colors.green,
-                    onPressed: (){
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => TicketPage()),
-                      );
-                    },
-                    child: Text(
-                      '1 pm',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),)),
-                  TableCell(child: Center(child: RaisedButton(
-                    color: Colors.green,
-                    onPressed: (){},
-                    child: Text(
-                      '2 pm',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),)),
-                ]),
-                TableRow(children: [
-                  TableCell(child: Center(child: RaisedButton(
-                    color: Colors.red,
-                    onPressed: (){},
-                    child: Text(
-                      '3 pm',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),)),
-                  TableCell(child: Center(child: RaisedButton(
-                    color: Colors.green,
-                    onPressed: (){},
-                    child: Text(
-                      '11 am',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),)),
-                  TableCell(child: Center(child: RaisedButton(
-                    color: Colors.green,
-                    onPressed: (){},
-                    child: Text(
-                      '4 pm',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),)),
-                  TableCell(child: Center(child: RaisedButton(
-                    color: Colors.red,
-                    onPressed: (){},
-                    child: Text(
-                      '5 pm',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),)),
-                ])
-                ],
-
-
-    )));
+        body: _loaded == false
+            ? Text("loading..")
+            : Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Table(
+                  //          defaultColumnWidth:
+                  //              FixedColumnWidth(MediaQuery.of(context).size.width / 3),
+                  border: TableBorder.all(
+                      color: Colors.black26, width: 1, style: BorderStyle.none),
+                  children: [
+                    TableRow(
+                      children: times.map((String slot) {
+                        return _timeCell(slot);
+                      }).toList(),
+                    )
+                  ],
+                )));
   }
 }

74d1cf17d20ea6a7a7d91ec46e7450dd49498b99
avani17101 <staravani1999@gmail.com>
2020-08-02 12:39:48 +0530

    display table no on type: sit in lib

--- libmate/lib/views/ticket.dart
+++ libmate/lib/views/ticket.dart
@@ -32,47 +32,49 @@ class _TicketPageState extends State<TicketPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<UserModel>(
-        builder: (BuildContext context, UserModel model, Widget child) {
-          var statsGroup = SliverToBoxAdapter(
-            child: Padding(
-                padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 30.0),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Row(children: [
-                        Text('Slot booked',
-                            style: TextStyle(
-                                color: Colors.black87,
-                                letterSpacing: 2.0,
-                                fontSize: 28.0,
-                                fontWeight: FontWeight.bold)),
-                        Spacer(),
-                        Column(children: [
-                          Text('Time',
-                              style: TextStyle(
-                                color: Colors.grey,
-                                letterSpacing: 2.0,
-                              )),
-                          Text('$time',
-                              style: TextStyle(
-                                  color: Colors.pinkAccent,
-                                  letterSpacing: 2.0,
-                                  fontSize: 28.0,
-                                  fontWeight: FontWeight.bold)),
-                        ])
-                      ]),
-                      Row(children: <Widget>[
-                        Icon(
-                          Icons.date_range,
-                          color: Colors.grey[400],
-                        ),
-                        Text('Date',
-                            style: TextStyle(
-                                color: Colors.grey[600],
-                                letterSpacing: 2.0,
-                                fontWeight: FontWeight.bold)),
-                      ]),
+    if(type==1)
+      {
+        return Consumer<UserModel>(
+            builder: (BuildContext context, UserModel model, Widget child) {
+              var statsGroup = SliverToBoxAdapter(
+                child: Padding(
+                    padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 30.0),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Row(children: [
+                            Text('Slot booked',
+                                style: TextStyle(
+                                    color: Colors.black87,
+                                    letterSpacing: 2.0,
+                                    fontSize: 28.0,
+                                    fontWeight: FontWeight.bold)),
+                            Spacer(),
+                            Column(children: [
+                              Text('Time',
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    letterSpacing: 2.0,
+                                  )),
+                              Text('$time',
+                                  style: TextStyle(
+                                      color: Colors.pinkAccent,
+                                      letterSpacing: 2.0,
+                                      fontSize: 28.0,
+                                      fontWeight: FontWeight.bold)),
+                            ])
+                          ]),
+                          Row(children: <Widget>[
+                            Icon(
+                              Icons.date_range,
+                              color: Colors.grey[400],
+                            ),
+                            Text('Date',
+                                style: TextStyle(
+                                    color: Colors.grey[600],
+                                    letterSpacing: 2.0,
+                                    fontWeight: FontWeight.bold)),
+                          ]),
 
 
                           Text('$date',
@@ -80,38 +82,106 @@ class _TicketPageState extends State<TicketPage> {
                                 color: Colors.grey,
                                 letterSpacing: 2.0,
                               )),
-                    ]
-                )
-            ),
-          );
-          var tableGroup = SliverToBoxAdapter(
-            child: Padding(
-                padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 30.0),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
+                        ]
+                    )
+                ),
+              );
+              var tableGroup = SliverToBoxAdapter(
+                child: Padding(
+                    padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 30.0),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
                           Text('\n\n Table Number: $tableNo',
                               style: TextStyle(
-                                color: Colors.pink,
-                                letterSpacing: 2.0,
+                                  color: Colors.pink,
+                                  letterSpacing: 2.0,
                                   fontWeight: FontWeight.bold
                               )),
-                    ]
-                )
-            ),
-          );
+                        ]
+                    )
+                ),
+              );
 
 
-          return Scaffold(
-              backgroundColor: Colors.grey[100],
-              appBar: new AppBar(
-                title: new Text('Ticket'),
-                centerTitle: true,
+              return Scaffold(
+                  backgroundColor: Colors.grey[100],
+                  appBar: new AppBar(
+                    title: new Text('Ticket'),
+                    centerTitle: true,
+                  ),
+                  drawer: AppDrawer(),
+                  body: CustomScrollView(
+                    slivers: [statsGroup, tableGroup],
+                  ));
+            });
+      }
+    else{
+      return Consumer<UserModel>(
+          builder: (BuildContext context, UserModel model, Widget child) {
+            var statsGroup = SliverToBoxAdapter(
+              child: Padding(
+                  padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 30.0),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Row(children: [
+                          Text('Slot booked',
+                              style: TextStyle(
+                                  color: Colors.black87,
+                                  letterSpacing: 2.0,
+                                  fontSize: 28.0,
+                                  fontWeight: FontWeight.bold)),
+                          Spacer(),
+                          Column(children: [
+                            Text('Time',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  letterSpacing: 2.0,
+                                )),
+                            Text('$time',
+                                style: TextStyle(
+                                    color: Colors.pinkAccent,
+                                    letterSpacing: 2.0,
+                                    fontSize: 28.0,
+                                    fontWeight: FontWeight.bold)),
+                          ])
+                        ]),
+                        Row(children: <Widget>[
+                          Icon(
+                            Icons.date_range,
+                            color: Colors.grey[400],
+                          ),
+                          Text('Date',
+                              style: TextStyle(
+                                  color: Colors.grey[600],
+                                  letterSpacing: 2.0,
+                                  fontWeight: FontWeight.bold)),
+                        ]),
+
+
+                        Text('$date',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              letterSpacing: 2.0,
+                            )),
+                      ]
+                  )
               ),
-              drawer: AppDrawer(),
-              body: CustomScrollView(
-                slivers: [statsGroup, tableGroup],
-              ));
-        });
+            );
+
+            return Scaffold(
+                backgroundColor: Colors.grey[100],
+                appBar: new AppBar(
+                  title: new Text('Ticket'),
+                  centerTitle: true,
+                ),
+                drawer: AppDrawer(),
+                body: CustomScrollView(
+                  slivers: [statsGroup],
+                ));
+          });
+    }
+
   }
 }

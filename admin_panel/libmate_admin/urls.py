dbff9ce31cc62f2d1666b324e2429eb14887ac38
KanishAnand <therealkanish@gmail.com>
2020-08-02 23:21:07 +0530

    admin dashboard

---
+++ admin_panel/libmate_admin/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from libmate_admin import views
+
+urlpatterns = [
+    path('', views.hello_world, name='hello_world'),
+    path('dashboard', views.dashboard, name='dashboard'),
+]
--- admin_panel/libmate_admin/views.py
+++ admin_panel/libmate_admin/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def hello_world(request):
+    return render(request, 'hello_world.html', {})
+
+def dashboard(request):
+    return render(request, 'dashboard.html', {})

3d7bfb76860ec0c3e0cd715c438fc8bad88fb862
avani17101 <staravani1999@gmail.com>
2020-08-02 16:43:30 +0530

    print screenshot

--- libmate/lib/generated_plugin_registrant.dart
+++ libmate/lib/generated_plugin_registrant.dart
@@ -9,6 +9,7 @@ import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:fluttertoast/fluttertoast_web.dart';
 import 'package:google_sign_in_web/google_sign_in_web.dart';
+import 'package:printing/src/printing_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 
@@ -20,6 +21,7 @@ void registerPlugins(PluginRegistry registry) {
   FirebaseCoreWeb.registerWith(registry.registrarFor(FirebaseCoreWeb));
   FluttertoastWebPlugin.registerWith(registry.registrarFor(FluttertoastWebPlugin));
   GoogleSignInPlugin.registerWith(registry.registrarFor(GoogleSignInPlugin));
+  PrintingPlugin.registerWith(registry.registrarFor(PrintingPlugin));
   SharedPreferencesPlugin.registerWith(registry.registrarFor(SharedPreferencesPlugin));
   UrlLauncherPlugin.registerWith(registry.registrarFor(UrlLauncherPlugin));
   registry.registerMessageHandler();
--- libmate/lib/views/ticket.dart
+++ libmate/lib/views/ticket.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:libmate/datastore/model.dart';
 import 'package:libmate/views/drawer.dart';
-import 'package:libmate/widgets/issueitem.dart';
 import 'package:provider/provider.dart';
 import 'package:qr_flutter/qr_flutter.dart';
-import 'package:libmate/datastore/state.dart';
 import 'dart:convert';
+import 'dart:ui';
+import 'package:flutter/rendering.dart';
+import 'dart:typed_data';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:simple_permissions/simple_permissions.dart';
+import 'dart:io';
+import 'dart:async';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+const directoryName = 'Signature';
 
 void printWrapped(String text) {
   final pattern = RegExp('.{1,800}'); // 800 is the size of each chunk
@@ -30,7 +42,6 @@ class TicketPage extends StatefulWidget {
       qrdata = jsonEncode(datalist);
       qrdata = jsonEncode(datalist);
 
-       printWrapped(qrdata);
   }
 
   @override
@@ -42,6 +53,67 @@ class _TicketPageState extends State<TicketPage> {
   String date;
   int tableNo;
   int type;
+  String _platformVersion = 'Unknown';
+  Permission _permission = Permission.WriteExternalStorage;
+  GlobalKey globalKey = GlobalKey();
+
+
+  Future _capturePng() async {
+    RenderRepaintBoundary boundary = globalKey.currentContext.findRenderObject();
+
+    if (boundary.debugNeedsPaint) {
+      print("Waiting for boundary to be painted.");
+      await Future.delayed(const Duration(milliseconds: 20));
+      return _capturePng();
+    }
+
+    var image = await boundary.toImage();
+    var byteData = await image.toByteData(format: ImageByteFormat.png);
+    return byteData.buffer.asUint8List();
+  }
+
+// Platform messages are asynchronous, so we initialize in an async method.
+  initPlatformState() async {
+    String platformVersion;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      platformVersion = await SimplePermissions.platformVersion;
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _platformVersion = platformVersion;
+    });
+    print(_platformVersion);
+  }
+
+  void _printPngBytes() async {
+    var pngBytes = await _capturePng();
+    var bs64 = base64Encode(pngBytes);
+    print(pngBytes);
+    print(bs64);
+// requesting external storage permission
+    if(!(await checkPermission())) await requestPermission();
+
+// Use plugin [path_provider] to export image to storage
+    Directory directory = await getExternalStorageDirectory();
+    String path = directory.path;
+    print(path);
+
+// create directory on external storage
+    await Directory('$path/$directoryName').create(recursive: true);
+
+// write to storage as a filename.png
+    File('$path/$directoryName/filename.png')
+        .writeAsBytesSync(pngBytes.buffer.asInt8List());
+  }
+
 
   _TicketPageState();
   @override
@@ -51,13 +123,35 @@ class _TicketPageState extends State<TicketPage> {
     time = widget.slot.split('_')[1];
     tableNo = widget.tableNo;
     type = widget.type;
+    initPlatformState();
   }
+
+
+
+  checkPermission() async {
+    bool result = await SimplePermissions.checkPermission(_permission);
+    return result;
+  }
+
+  getPermissionStatus() async {
+    final result = await SimplePermissions.getPermissionStatus(_permission);
+    print("permission status is " + result.toString());
+  }
+  requestPermission() async {
+    final result = await SimplePermissions.requestPermission(_permission);
+    return result;
+  }
+
+
+
 
   @override
   Widget build(BuildContext context) {
     if(type==1)
       {
-        return Consumer<UserModel>(
+        return RepaintBoundary(
+            key: globalKey,
+            child: Consumer<UserModel>(
             builder: (BuildContext context, UserModel model, Widget child) {
               var statsGroup = SliverToBoxAdapter(
                 child: Padding(
@@ -109,7 +203,6 @@ class _TicketPageState extends State<TicketPage> {
 
                         QrImage(data: widget.qrdata, version: QrVersions.auto, size: 200.0),
                         Text("Show this QR code to the library admin"),
-                        Text("Not received confirmation from admin yet")
 
 
                         ]
@@ -140,8 +233,11 @@ class _TicketPageState extends State<TicketPage> {
                     )
                 ),
               );
-
-
+              var printGroup = SliverToBoxAdapter(
+                child: RaisedButton(
+                  child: const Text('Print'),
+                  onPressed: _printPngBytes),
+              );
               return Scaffold(
                   backgroundColor: Colors.grey[100],
                   appBar: new AppBar(
@@ -150,9 +246,9 @@ class _TicketPageState extends State<TicketPage> {
                   ),
                   drawer: AppDrawer(),
                   body: CustomScrollView(
-                    slivers: [statsGroup, tableGroup],
+                    slivers: [statsGroup, tableGroup,printGroup],
                   ));
-            });
+            }));
       }
     else{
       return Consumer<UserModel>(
@@ -189,7 +285,6 @@ class _TicketPageState extends State<TicketPage> {
                                     fontWeight: FontWeight.bold)),
                           ])
                         ]),
-
                         Row(children: <Widget>[
                           Icon(
                             Icons.date_range,
@@ -201,9 +296,6 @@ class _TicketPageState extends State<TicketPage> {
                                   letterSpacing: 2.0,
                                   fontWeight: FontWeight.bold)),
                         ]),
-
-
-
                         Text('$date',
                             style: TextStyle(
                               color: Colors.grey,
@@ -221,13 +313,14 @@ class _TicketPageState extends State<TicketPage> {
                             ]),
                         QrImage(data: widget.qrdata, version: QrVersions.auto, size: 200.0),
                         Text("Show this QR code to the library admin"),
-
-
-
-
                       ]
                   )
               ),
+            );
+            var printGroup = SliverToBoxAdapter(
+              child: RaisedButton(
+                  child: const Text('Print Screenshot'),
+                  onPressed: _printPngBytes),
             );
 
             return Scaffold(
@@ -238,7 +331,7 @@ class _TicketPageState extends State<TicketPage> {
                 ),
                 drawer: AppDrawer(),
                 body: CustomScrollView(
-                  slivers: [statsGroup],
+                  slivers: [statsGroup,printGroup],
                 ));
           });
     }
